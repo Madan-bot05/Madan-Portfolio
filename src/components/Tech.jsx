a41e2cbc4aa19f51f8cb2fdc58a00f84f49a39bd
Madan-bot05 <madanmohandas983@gmail.com>
2024-09-08 20:38:50 +0530

    Completed the technology section

---
+++ src/components/Tech.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { RiJavaLine, RiReactjsLine } from 'react-icons/ri'
+import { BiLogoSpringBoot } from "react-icons/bi";
+import { GrMysql } from "react-icons/gr";
+import { LiaDocker } from "react-icons/lia";
+import { FaGitAlt } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { SiKubernetes } from "react-icons/si";
+
+const Tech = () => {
+  return (
+    <div className='border-b border-neutral-800 pb-24'>
+        <h1 className='my-20 text-center text-4xl'>Technologies</h1>
+        <div className='flex flex-wrap items-center justify-center gap-4'>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                <RiReactjsLine className='text-7xl text-cyan-700'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                <RiJavaLine className='text-7xl text-red-800'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                <BiLogoSpringBoot  className='text-7xl text-green-400'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                < GrMysql className='text-7xl text-cyan-800'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                <LiaDocker className='text-7xl text-cyan-500'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                <FaGitAlt className='text-7xl text-red-800'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent p-4'>
+                < SiJavascript className='text-7xl text-yellow-500'/>
+            </div>
+            <div className='rounded-2xl border-4 border-transparent'>
+                <SiKubernetes className='text-7xl text-blue-500'/>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Tech
