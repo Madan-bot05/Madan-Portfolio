fe8ab657bb524f6c15daac7bbf0a74168b4fceb0
Madan-bot05 <madanmohandas983@gmail.com>
2024-09-24 20:22:52 +0530

    working on animation in education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { EDUCATION } from '../constants'
+import { motion } from "framer-motion"
 
 const Education = () => {
   return (
     <div className='py-9 border-b border-neutral-900 ' id='education'>
-        <h2 className='mb-4 text-center text-3xl font-semibold'>Education</h2>
+        <motion.h2 whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className='mb-4 text-center text-3xl font-semibold'>Education</motion.h2>
         {EDUCATION.map((edu,index)=>(
             <div key={index} className='text-center mb-6 p-10'>
                 <h3 className='text-xl font-semibold'>{edu.degree}</h3>
