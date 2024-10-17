0dd527c9b5df798194ce9bc9d287428fff82fbbe
Madan-bot05 <madanmohandas983@gmail.com>
2024-10-17 19:28:13 +0530

    Added resume button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import aboutImg from '../assets/about.jpg'
 import { ABOUT_TEXT } from '../constants'
 import { motion } from "framer-motion"
+import { MdArrowOutward } from 'react-icons/md' // Import the icon
 
 const About = () => {
   return (
@@ -17,13 +18,26 @@ const About = () => {
                 <img className='rounded-2xl' src={aboutImg} alt="" />
             </div>
             </motion.div>
+
             <motion.div
              whileInView={{opacity:1,x:0}}
              initial={{opacity:0,x:100}}
-             transition={{duration:0.5,duration:0.5}}
-              className='w-full lg:w-1/2 text-center '>
-            <div className='flex justify-center lg:justify-start'>
+             transition={{duration:0.5}}
+              className='w-full lg:w-1/2 text-center relative'>
+            <div className='flex justify-center lg:justify-start flex-col items-center lg:items-start'>
                 <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+                <div className="px-[14rem] w-full flex justify-start mt-auto">
+                  <motion.a 
+                               whileInView={{opacity:1,x:0}}
+                               initial={{opacity:0,x:100}}
+                               transition={{duration:0.5}}
+                  href='https://drive.google.com/file/d/1kClOJ6YUlctZcjLVQb4r8YflT0XdER0J/view?usp=drive_link' target="_blank" rel="noopener noreferrer">
+                  <button className='flex items-center px-4 py-2 bg-gray-700 text-white rounded-lg'>
+                    Resume
+                    <MdArrowOutward className='ml-2' /> {/* Icon with margin */}
+                  </button>
+                  </motion.a>
+                </div>
             </div>
             </motion.div>
         </div>
